6399de86bd44159b7e14aad2aec1b405250808a8
Victor Wang <62414824+Victor-wang-902@users.noreply.github.com>
2024-02-02 02:28:48 +0800

    Delete SUREALM directory

--- SUREALM/src/modeling/__init__.py
+++
@@ -1,3 +0,0 @@
-from .models_bert import RetrievalModel
-from .models_gpt2 import RetrievalGPT2Model
-from .models_roberta import RetrievalModelRoberta
--- SUREALM/src/wrappers/__init__.py
+++
@@ -1,3 +0,0 @@
-from .wrappers_gpt2 import RetrievalGenerationModelGPT2, RetrievalGeneratorGPT2
-from .wrappers_roberta import RetrievalGenerationModelRoberta, RetrievalGeneratorRoberta
-from .wrappers import RetrievalGenerationModel, RetrievalGenerator
